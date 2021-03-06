ab1ad39b4e259587d470c0616cae64f27eb5b2a0
Luciano Martins <zicadopv@gmail.com>
2021-03-05 22:13:25 -0300

    First Commit

--- loja-fone/js/home.js
+++
@@ -1,9 +0,0 @@
-let valorInicial = 10;
-const $second = document.querySelector(".-second");
-
-$second.addEventListener("click", handleClick);
-function handleClick() {
-  const $carrinho = document.querySelector(".-last");
-
-  $carrinho.textContent = `Carrinho (${++valorInicial})`;
-}
